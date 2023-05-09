9967f3806723a1697f8a3f92e068aae14ae603b8
EvaLukasova <129587713+EvaLukasova@users.noreply.github.com>
2023-05-09 20:39:14 +0200

    Add files via upload

--- DU6_2.py
+++ DU6_2.py
@@ -1,2 +1,2 @@
 """ Zkopíruj si obsah souboru posta.txt do regex101 jako testovací řetězec. Vymysli regulární výraz, který označí všechny "poslední řádky adresy" v textu. Poslední řádka adresy zpravidla obsahuje PSČ a název obce, například 190 16 PRAHA 916 nebo 742 45 FULNEK. Celkem by jich mělo být 18. """
-([1-7][0-9]{2})(\ )(\d{2})(\ )(\ ?)(\w)
+([1-7][0-9]{2})(\ )(\d{2})(\ )((\ ?)(\w+)){3}
